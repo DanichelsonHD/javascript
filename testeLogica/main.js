31a50d26e14427083273cfa5cc6639f7cbee4b1e
DanichelsonHD <122651033+DanichelsonHD@users.noreply.github.com>
2024-09-13 18:32:33 -0300

    Update main.js

--- testeLogica/main.js
+++ testeLogica/main.js
@@ -1,13 +1,16 @@
 var numbersQuantity = 10;
 var averageQuantity = 3;
-let numbersList = [8, 2, 9, 11, 3, 1, 7, 6, 12, 4, 5, 10];
+var x = document.querySelector("#xNumber");
+var y = document.querySelector("#yNumber");
+var newNumberList = document.querySelector("#newNumber");
+let numbersList = [3,18, 6, 21, 9, 24, 12, 33, 15, 27, 36, 30];
 let averageList = [];
 let whiteList;
 
-calculateAverage();
-
 function calculateAverage ()
 {
+    setNumbers();
+    document.querySelector("#list").innerHTML = "[" + numbersList + "]";    
     validate();
     getWantedNumbers();
     while(canCalculate())
@@ -15,8 +18,8 @@ function calculateAverage ()
         calcAverage_pushNumber();
         whiteList.shift();
     }
-    console.log(averageList);
-    averageList = [];
+    writeOnConsole();
+    document.querySelector("#result").innerHTML = "[" + averageList + "]";
 }
 
 function canCalculate ()
@@ -46,4 +49,42 @@ function  validate ()
     if(numbersList == null || typeof(numbersList) != "object") throw new Error("Lista Inválida");
     if(numbersQuantity == null || typeof(numbersQuantity) != "number") throw new Error("Quantia de Números Inválida");
     if(averageQuantity == null || typeof(averageQuantity) != "number") throw new Error("Número da Média Inválido");
+
+    if(numbersQuantity > numbersList.length) throw new Error("Quantia de Números maior do que os presentes na Lista");
+    if(numbersQuantity < averageQuantity) throw new Error("Quantia de Números menos do que a Média");
+    if(averageQuantity > numbersList.length) throw new Error("Média maior do que a Lista");
+}
+
+function writeOnConsole ()
+{
+    console.clear();
+    console.log("X: " + numbersQuantity);
+    console.log("Y: " + averageQuantity);
+    console.log("Lista original: " + numbersList);
+    console.log("Lista de médias: " + averageList);
+}
+
+function setNumbers() 
+{
+    var numberX = parseInt(x.value);
+    var numberY = parseInt(y.value);
+    var numberList = parseInt(newNumberList.value);
+    
+    calculateNewNumbers(numberX, numberY, numberList);
+
+    averageList = [];
+    whiteList = [];
+}
+
+function calculateNewNumbers(numberX, numberY, numberList)
+{
+    if(!isNaN(numberX)) numbersQuantity = numberX; else numbersQuantity = 10;
+    if(!isNaN(numberY)) averageQuantity = numberY; else averageQuantity = 3;
+    if(!isNaN(numberList)) numbersList.push(numberList); 
+    if(numbersList == null && typeof(numbersList) != "object") numbersList = [3,18, 6, 21, 9, 24, 12, 33, 15, 27, 36, 30];
+}
+
+function resetList()
+{
+    numbersList = [];
 }
